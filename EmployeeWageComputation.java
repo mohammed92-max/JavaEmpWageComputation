baf91a910d01b248098369ef07f62277969ca915
mohammed92-max <shahrukhkhandb@gmail.com>
2020-05-10 18:00:09 +0530

    pushed UseCase1

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,7 +1,19 @@
 public class EmployeeWageComputation
 {
-	public static void main( string[] argv)
+	public static void main (String[] args)
 	{
-		System.out.println("Welcome to Employee Wage Computation Program");
+		int IS_FULL_TIME=1;
+
+		double empCheck=Math.floor(Math.random()*10) % 2;
+
+		if(empCheck == IS_FULL_TIME)
+		{
+			System.out.println("Employee present");
+		}
+		else
+		{
+			System.out.println("Employee absent");
+		}
 	}
 }
+
