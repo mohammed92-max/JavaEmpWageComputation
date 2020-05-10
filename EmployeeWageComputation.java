34f4ad1fd1d99b6f26681225b8dcf90068b42ab4
mohammed92-max <shahrukhkhandb@gmail.com>
2020-05-10 21:19:16 +0530

    pushed UseCase4

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,16 +1,34 @@
+import java.util.Scanner;
+
 public class EmployeeWageComputation
 {
 	public static void main (String[] args)
 	{
-		int wagePerHour=20,totalHoursPerDay=8,dailyWage;
+		int wagePerHour=20,totalHoursPerDay=8,fullTimeDailyWage;
 		int partTimeEmp,partTimeWage,partTimeHour=8;
 
-		dailyWage = wagePerHour * totalHoursPerDay;
-		int partTimeEmpDailyWage = partTimeHour * wagePerHour;
+		System.out.println("1) Part time wage");
+		System.out.println("2) Full time wage");
 
-		System.out.println("Daily wage is "+ dailyWage);
-		System.out.println("Part time employee daily wage is "
-				    +partTimeEmpDailyWage);
+		Scanner scan = new Scanner(System.in);		
+		System.out.println("Enter choice ");
+		int choice = scan.nextInt();
+		
+		switch(choice)
+		{
+
+		case 1: int partTimeEmpDailyWage = partTimeHour * wagePerHour;
+			System.out.println("Part time employee daily wage is "
+                        +partTimeEmpDailyWage);
+			break;
+
+		case 2: fullTimeDailyWage = wagePerHour * totalHoursPerDay;
+	                System.out.println("Daily wage is "+ fullTimeDailyWage);
+			break;
+
+		default: System.out.println("Invalid input");
+			 break;
+		}
 	}
 }
 
