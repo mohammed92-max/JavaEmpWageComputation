786d97c9bbbbdd6e87feb038699a38c97fe98628
mohammed92-max <shahrukhkhandb@gmail.com>
2020-05-10 20:51:11 +0530

    pushed UseCase3

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,9 +3,14 @@ public class EmployeeWageComputation
 	public static void main (String[] args)
 	{
 		int wagePerHour=20,totalHoursPerDay=8,dailyWage;
+		int partTimeEmp,partTimeWage,partTimeHour=8;
 
 		dailyWage = wagePerHour * totalHoursPerDay;
+		int partTimeEmpDailyWage = partTimeHour * wagePerHour;
+
 		System.out.println("Daily wage is "+ dailyWage);
+		System.out.println("Part time employee daily wage is "
+				    +partTimeEmpDailyWage);
 	}
 }
 
