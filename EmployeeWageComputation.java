e3a8c425f6814c9fcbf9554047d8595aa895b789
mohammed92-max <shahrukhkhandb@gmail.com>
2020-05-10 21:40:18 +0530

    pushed UseCase5

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,8 +4,9 @@ public class EmployeeWageComputation
 {
 	public static void main (String[] args)
 	{
-		int wagePerHour=20,totalHoursPerDay=8,fullTimeDailyWage;
+		int wagePerHour=20,totalHoursPerDay=8,fullTimeDailyWage=0;
 		int partTimeEmp,partTimeWage,partTimeHour=8;
+		int partTimeEmpDailyWage=0;
 
 		System.out.println("1) Part time wage");
 		System.out.println("2) Full time wage");
@@ -17,12 +18,20 @@ public class EmployeeWageComputation
 		switch(choice)
 		{
 
-		case 1: int partTimeEmpDailyWage = partTimeHour * wagePerHour;
+		case 1: for(int i=1;i<=20;i++)
+			{
+	   		partTimeEmpDailyWage = partTimeEmpDailyWage + partTimeHour * wagePerHour;
+			}
+
 			System.out.println("Part time employee daily wage is "
                         +partTimeEmpDailyWage);
 			break;
 
-		case 2: fullTimeDailyWage = wagePerHour * totalHoursPerDay;
+		case 2: for(int j=1;j<=20;j++)
+			{
+			fullTimeDailyWage = fullTimeDailyWage + wagePerHour * totalHoursPerDay;
+			}
+			
 	                System.out.println("Daily wage is "+ fullTimeDailyWage);
 			break;
 
