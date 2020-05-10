f7c1ac6dbe24323fa2ca8743366f296beb013ac9
mohammed92-max <shahrukhkhandb@gmail.com>
2020-05-10 18:18:25 +0530

    pushed UseCase2

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,18 +2,15 @@ public class EmployeeWageComputation
 {
 	public static void main (String[] args)
 	{
-		int IS_FULL_TIME=1;
+		int wagePerHour=20,totalHoursPerDay=8,dailyWage;
 
-		double empCheck=Math.floor(Math.random()*10) % 2;
-
-		if(empCheck == IS_FULL_TIME)
-		{
-			System.out.println("Employee present");
-		}
-		else
-		{
-			System.out.println("Employee absent");
-		}
+		dailyWage = wagePerHour * totalHoursPerDay;
+		System.out.println("Daily wage is "+ dailyWage);
 	}
 }
+
+
+
+
+
 
