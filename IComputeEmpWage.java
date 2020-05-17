f8891410dcf285ae5eb86ba33a20214bed64a9f8
mohammed92-max <shahrukhkhandb@gmail.com>
2020-05-17 09:36:30 +0530

    pushed Day23UseCase11

---
+++ IComputeEmpWage.java
@@ -0,0 +1,8 @@
+package UC11;
+public interface IComputeEmpWage 
+{
+	public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth);
+	public void computeEmpWage();
+	public int getTotalWage(String company);
+}
+
