c74168dae16be83f68836884177681a92abcedde
mohammed92-max <shahrukhkhandb@gmail.com>
2020-05-17 09:48:56 +0530

    pushed Day23UseCase12

--- EmpWageBuilderArray.java
+++ EmpWageBuilderArray.java
@@ -1,36 +1,51 @@
-public class EmpWageBuilder 
+package UC12;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class EmpWageBuilder implements IComputeEmpWage
 {
 	public static final int PART_TIME = 1;
 	public static final int FULL_TIME = 2;
 	
-	private int numOfCompany = 0;
-	private CompanyEmpWage[] EmployeeWageComputationUC10Array;
+	//private int numOfCompany=0;
+	private ArrayList<CompanyEmpWage> companyEmpWageList;
+	private Map<String,CompanyEmpWage> companyToEmpWageMap;
 	
 	public EmpWageBuilder()
 	{
-		EmployeeWageComputationUC10Array = new CompanyEmpWage[5];
+		companyEmpWageList = new ArrayList<>();
+		companyToEmpWageMap = new HashMap<>();
 	}
 	
-	private void addEmployeeWageComputationUC10(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
+	public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
 	{
-		EmployeeWageComputationUC10Array[numOfCompany] = new CompanyEmpWage(company,empRatePerHour,numOfWorkingDays,maxHoursPerMonth);
-		numOfCompany++;
+		CompanyEmpWage companyEmpWage = new CompanyEmpWage(company,empRatePerHour,numOfWorkingDays,maxHoursPerMonth);
+		companyEmpWageList.add(companyEmpWage);
+		companyToEmpWageMap.put(company,companyEmpWage);
 	}
 	
-	private void computeEmpWage()
+	public void computeEmpWage()
 	{
-		for(int i=0; i<numOfCompany; i++)
+		for(int i=0; i< companyEmpWageList.size(); i++)
 		{
-			EmployeeWageComputationUC10Array[i].setTotalEmpWage(this.computeEmpWage(EmployeeWageComputationUC10Array[i]));
-			System.out.println(EmployeeWageComputationUC10Array[i]);
+			CompanyEmpWage companyEmpWage = companyEmpWageList.get(i);
+			companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+			System.out.println(companyEmpWage);
 		}
 	}
 	
-	private int computeEmpWage(CompanyEmpWage employeeWageComputationUC10)
+	public int getTotalWage(String company)
+	{
+		return companyToEmpWageMap.get(company).totalEmpWage;
+	}
+	
+	public int computeEmpWage(CompanyEmpWage companyEmpWage)
 	{
 		int empHrs=0,totalWorkingDays=0,totalEmpHrs=0;
 		
-		while(totalEmpHrs<= employeeWageComputationUC10.maxHoursPerMonth && totalWorkingDays < employeeWageComputationUC10.numOfWorkingDays)
+		while(totalEmpHrs<= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays)
 		{
 			totalWorkingDays++;
 			int checkEmp = (int)Math.floor(Math.random()*10) % 3;																	
@@ -49,15 +64,17 @@ public class EmpWageBuilder
 			totalEmpHrs = totalEmpHrs + empHrs; 	
 			System.out.println("Day "+totalWorkingDays+ " Emp Hr "+empHrs);
 		}
-		return totalEmpHrs * employeeWageComputationUC10.empRatePerHour;
+		return totalEmpHrs * companyEmpWage.empRatePerHour;		
+		
 	}
 	
-	public static void main (String[] args)
+	public static void main(String[] args)
 	{
-		EmpWageBuilder empWageBuilder = new EmpWageBuilder();
-		empWageBuilder.addEmployeeWageComputationUC10("BigBazaar",20,22,176);
-		empWageBuilder.addEmployeeWageComputationUC10("DMart",10,22,198);
+		IComputeEmpWage empWageBuilder = new EmpWageBuilder();
+		empWageBuilder.addCompanyEmpWage(" DMart",20,2,10);
+		empWageBuilder.addCompanyEmpWage(" Reliance",10,4,20);
 		empWageBuilder.computeEmpWage();
 	}
 }
+
 
